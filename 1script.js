8692d5b298a77fdb8e6cb2923d121e73ef02f51c
Panchami <panchamimestha702@gmail.com>
2026-01-24 11:59:01 +0530

    Updated code

--- 1script.js
+++ 1script.js
@@ -1,4 +1,4 @@
 function login() {
-  document.getElementById("loginPage").style.display = "none";
-  document.getElementById("directoryPage").style.display = "block";
+document.getElementById("loginPage").style.display = "none";
+document.getElementById("directoryPage").style.display = "block";
 }
